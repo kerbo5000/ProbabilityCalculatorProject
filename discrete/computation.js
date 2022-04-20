1b8a59f0f2fef7b0eda1ffaa44d0b10ec25ff89b
kerbo5000 <kerbysd@hotmail.com>
2022-04-20 19:23:15 -0400

    input validation done for discrete

--- discrete/computation.js
+++ discrete/computation.js
@@ -5,37 +5,20 @@ const pdf = document.getElementById('pmf');
 const cdf = document.getElementById('cdf');
 const dist = document.getElementById('dist');
 const message = document.createElement('h3');
-let meanValue;
-let varianceValue;
-let pmfValue;
-let cdfValue;
+const meanValue = document.createElement('span');
+const varianceValue = document.createElement('span');
+const pmfValue = document.createElement('span');
+const cdfValue = document.createElement('span');
+let setValues = false;
 submit.addEventListener("click",result);
 function result(e){
   e.preventDefault();
-  // let xValue = x.value;
-  // let nValue= n.value;
-  // let pValue = probability.value;
-  // if(nValue<= 0){
-  //   message.innerText = "the value for n has to be a positive integer";
-  //   submit.parentElement.appendChild(message);
-  //   return;
-  // }
-  // if(xValue< 0 || xValue > nValue ){
-  //   message.innerText = "the value for x has to be an integer between 0 and n";
-  //   submit.parentElement.appendChild(message);
-  //   return;
-  // }
-  // if(pValue < 0 || pValue> 1 ){
-  //   message.innerText = "the value for p has to be a number between 0 and 1";
-  //   submit.parentElement.appendChild(message);
-  //   return;
-  // }
+  message.innerText = "";
   switch(dist.textContent){
     case "Binomial":
       bin();
       break;
     case "Geometric":
-    console.log("HI");
       geo();
       break;
     case "Hypergeometric":
@@ -48,16 +31,20 @@ function result(e){
       poi();
       break;
   }
-  mean.appendChild(document.createTextNode(+(meanValue.toFixed(3))));
-  variance.appendChild(document.createTextNode(+(varianceValue.toFixed(3))));
-  pmf.appendChild(document.createTextNode(+(pmfValue.toFixed(3))));
-  cdf.appendChild(document.createTextNode(+(cdfValue.toFixed(3))));
+  submit.parentElement.appendChild(message);
+  setTimeout(function(){
+     submit.parentElement.removeChild(message)
+   },3000)
+  mean.appendChild(meanValue);
+  variance.appendChild(varianceValue);
+  pmf.appendChild(pmfValue);
+  cdf.appendChild(cdfValue);
   return;
 }
 
-function cdfunc(x,func){
+function cdfunc(x,func,s){
   let result = 0;
-  for(let i=0;i<=x;i++){
+  for(let i=s;i<=x;i++){
     result += func(i);
   }
   return result;
@@ -66,53 +53,109 @@ const fact = n => !(n>1) ? 1: fact(n-1)*n;
 const combi = (n,k) => (fact(n)/(fact(k)*fact(n-k)));
 
 function bin(){
-  const x = document.getElementById("x-value").value;
-  const n = document.getElementById("n-value").value;
-  const p = document.getElementById("p-value").value;
-  meanValue = n*p;
-  varianceValue = n*p*(1-p);
+  const x = Number(document.getElementById("x-value").value);
+  const n = Number(document.getElementById("n-value").value);
+  const p = Number(document.getElementById("p-value").value);
+  if(!Number.isInteger(n)||n<= 0){
+    message.innerText = "the value for n has to be a positive integer";
+    return;
+  }
+  if(!Number.isInteger(x)||x< 0 || x > n ){
+    message.innerText = "the value for x has to be an integer between 0 and n";
+    return;
+  }
+  if(p < 0 || p > 1 ){
+    message.innerText = "the value for p has to be a number between 0 and 1";
+    return;
+  }
+  meanValue.innerText = +((n*p).toFixed(3));
+  varianceValue.innerText = +((n*p*(1-p)).toFixed(3));
   const pmfunc = (i) =>combi(n,i)*p**i*(1-p)**(n-i);
-  pmfValue = pmfunc(x);
-  cdfValue= cdfunc(x,pmfunc);
+  pmfValue.innerText = +(pmfunc(x).toFixed(3));
+  cdfValue.innerText= +(cdfunc(x,pmfunc,0).toFixed(3));
 }
 function geo(){
-  const x = document.getElementById("x-value").value;
-  const p = document.getElementById("p-value").value;
-  meanValue = 1/p;
-  varianceValue = (1-p)/p**2;
-  const pmfunc = (i) => (i<=0)?0: p*(1-p)**(i-1);
-  pmfValue = pmfunc(x);
-  cdfValue= cdfunc(x,pmfunc);
+  const x = Number(document.getElementById("x-value").value);
+  const p = Number(document.getElementById("p-value").value);
+  if(!Number.isInteger(x)||x<= 0 ){
+    message.innerText = "the value for x has to be a positive integer";
+    return;
+  }
+  if(p < 0 || p> 1 ){
+    message.innerText = "the value for p has to be a number between 0 and 1";
+    return;
+  }
+  meanValue.innerText = +((1/p).toFixed(3));
+  varianceValue.innerText = +(((1-p)/p**2).toFixed(3));
+  const pmfunc = (i) => p*(1-p)**(i-1);
+  pmfValue.innerText = +(pmfunc(x).toFixed(3));
+  cdfValue.innerText= +(cdfunc(x,pmfunc,1).toFixed(3));
 }
 
 function hyp(){
-  const x = document.getElementById("x-value").value;
-  const n = document.getElementById("n-value").value;
-  const bigN = document.getElementById("N-value").value;
-  const bigK = document.getElementById("K-value").value;
-  meanValue = n*(bigK/bigN);
-  varianceValue = n*(bigK/bigN)*((bigN-bigK)/bigN)*((bigN-n)/(bigN-1));
+  const x = Number(document.getElementById("x-value").value);
+  const n = Number(document.getElementById("n-value").value);
+  const bigN = Number(document.getElementById("N-value").value);
+  const bigK = Number(document.getElementById("K-value").value);
+  if(!Number.isInteger(bigN)||bigN<= 0 ){
+    message.innerText = "the value for N has to be a positive integer";
+    return;
+  }
+  if(!Number.isInteger(bigk)||bigK<= 0||bigK>bigN ){
+    message.innerText = "the value for K has to be an integer between 1 and N";
+    return;
+  }
+  if(!Number.isInteger(n)||n<= 0||n>bigN){
+    message.innerText = "the value for n has to be an integer between 1 and N";
+    return;
+  }
+  if(!Number.isInteger(x)||x< 0 || x > bigK ){
+    message.innerText = "the value for x has to be an integer between 0 and K";
+    return;
+  }
+  meanValue.innerText = +((n*(bigK/bigN)).toFixed(3));
+  varianceValue.innerText = +((n*(bigK/bigN)*((bigN-bigK)/bigN)*((bigN-n)/(bigN-1))).toFixed(3));
   const pmfunc =(i)=> (combi(bigK,i)*combi(bigN-bigK,n-i))/combi(bigN,n);
-  pmfValue = pmfunc(x);
-  cdfValue= cdfunc(x,pmfunc);
+  pmfValue.innerText = +(pmfunc(x).toFixed(3));
+  cdfValue.innerText= +(cdfunc(x,pmfunc,0).toFixed(3));
 }
 
 function neg(){
-  const x = document.getElementById("x-value").value;
-  const p = document.getElementById("p-value").value;
-  const k = document.getElementById("k-value").value;
-  meanValue = k/p;
-  varianceValue = (k*(1-p))/p**2;
-  const pmfunc = (i)=>(i<k)?0: combi(i-1,k-1)*(1-p)**(i-k)*p**k;
-  pmfValue = pmfunc(x);
-  cdfValue= cdfunc(x,pmfunc);
+  const x = Number(document.getElementById("x-value").value);
+  const p = Number(document.getElementById("p-value").value);
+  const k = Number(document.getElementById("k-value").value);
+  if(!Number.isInteger(k)||k<= 0){
+    message.innerText = "the value for k has to be a positive integer";
+    return;
+  }
+  if(!Number.isInteger(x)||x< k){
+    message.innerText = "the value for x has to be an integer greater then k";
+    return;
+  }
+  if(p < 0 || p> 1 ){
+    message.innerText = "the value for p has to be a number between 0 and 1";
+    return;
+  }
+  meanValue.innerText = +((k/p).toFixed(3));
+  varianceValue.innerText = +(((k*(1-p))/p**2).toFixed(3));
+  const pmfunc = (i)=> combi(i-1,k-1)*(1-p)**(i-k)*p**k;
+  pmfValue.innerText = +(pmfunc(x).toFixed(3));
+  cdfValue.innerText= +(cdfunc(x,pmfunc,k).toFixed(3));
 }
 function poi(){
-  const l = document.getElementById("l-value").value;
-  const x = document.getElementById("x-value").value;
-  meanValue = l;
-  varianceValue = l;
+  const l = Number(document.getElementById("l-value").value);
+  const x = Number(document.getElementById("x-value").value);
+  if(!Number.isInteger(x)||x< 0){
+    message.innerText = "the value for x has to be a positive integer";
+    return;
+  }
+  if(l< 0){
+    message.innerText = "the value for l has to be a positive number";
+    return;
+  }
+  meanValue.innerText = +((l*1).toFixed(3));
+  varianceValue.innerText = +((l*1).toFixed(3));
   const pmfunc = (i)=> (l**i*Math.exp(-l))/fact(i);
-  pmfValue = pmfunc(x);
-  cdfValue= cdfunc(x,pmfunc);
+  pmfValue.innerText = +(pmfunc(x).toFixed(3));;
+  cdfValue.innerText= +(cdfunc(x,pmfunc,0).toFixed(3));
 }
