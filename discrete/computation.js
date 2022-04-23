83811ce2d1d5390bef2ab55af1caf643a22e0bca
kerbo5000 <kerbysd@hotmail.com>
2022-04-22 22:06:58 -0400

    got rid of continuous random variables done with project

--- discrete/computation.js
+++ discrete/computation.js
@@ -10,6 +10,9 @@ const varianceValue = document.createElement('span');
 const pmfValue = document.createElement('span');
 const cdfValue = document.createElement('span');
 let setValues = false;
+// fetch("https://quickchart.io/chart?c={type:'bar',data:{labels:[2012,2013,2014,2015,2016],datasets:[{label:'Users',data:[120,60,50,180,120]}]}}")
+// .then((res)=> res.text()
+// ))
 submit.addEventListener("click",result);
 function result(e){
   e.preventDefault();
